a4f2fb116bd13c7e550c9eacdb56ba6b53ac8906
Fikri-Alfarizi <fikrialfarizi039@gmail.com>
2025-12-30 22:56:10 +0700

    Chore: Migrate to production URL and force HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if($this->app->environment('production') || env('APP_URL') === 'https://allforme-production.up.railway.app') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         // Share notifications with all views (or specifically the header)
         \Illuminate\Support\Facades\View::composer('layouts.header', function ($view) {
             if (auth()->check()) {
